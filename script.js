a546cb384252971bdf7aca6dc5f009997dde1609
Karalyn Heath <karalynmh@gmail.com>
2015-07-09 18:09:42 -0700

    Set currentUser on login and register, set votesRemaining on account creation, created populateGameVotes function to load games current standings onload, got votesRemaining from database, verified current vote is valid, updated votesRemaining for user, updated votes for game voted on to database and game table.
    Signed-off-by: Karalyn Heath <karalynmh@gmail.com>

--- script.js
+++ script.js
@@ -35,42 +35,125 @@ Allow users to move the dialog boxes around
 //DEBUG flag
 var debug = true;
 
+var currentUser = null;
+
 window.onload = function() {
   if(debug){console.log("onload");}
   populateUserList();
+  populateGameVotes();
   eventHandlers();
   
 }; 
 
 var eventHandlers = function() {
-  for(var count = 0; count < document.getElementsByClassName('gamevote').length; count++){
-      document.getElementsByClassName('gamevote')[count].onclick = function() {
-        var voteNumber = this.className.split(" ")[1].split("-")[1];
-        var gameNode = this.parentNode.parentNode.parentNode;
-        if(debug){console.log("You voted "+voteNumber+" on the game "+ gameNode.id);}
+  if(debug){console.log("eventHandlers");}
 
-        //user will vote on game with a value 1-5
-        //before saving vote see if user has enough left
-        var Games = Parse.Object.extend("Games");
-        var query = new Parse.Query(Games);
-        var previousVote = 0;
-        query.get(gameNode.id, {
-          success: function(games) {
-            previousVote = games.get("vote");
-            var games = new Games();
-            games.save({objectId: gameNode.id, vote: previousVote + parseInt(voteNumber)});
-            gameNode.getElementsByClassName('votes')[0].innerHTML = previousVote + parseInt(voteNumber);
-          },
-          error: function(object, error) {
-            console.log(error);
+  //iterate through meeple voting buttons in game table
+  for(var count = 0; count < document.getElementsByClassName('gamevote').length; count++){
+
+      //setup onclick event handlers for each meeple voting button in game table
+      document.getElementsByClassName('gamevote')[count].onclick = function() {
+
+        //when voting meeple clicked get the number of votes user selected
+        //TODO: see if you can remove 'gamevote' class altogether and get value of the meeple without spitting the classname
+        var voteNumber = this.className.split(" ")[1].split("-")[1];
+        //get the row of the game voted on by traversing up the DOM of the parent nodes of meeple clicked
+        var gameNode = this.parentNode.parentNode.parentNode;
+        
+        //if user is logged in continue, else ask them to log in
+        if(currentUser){
+
+          //get votes user has remaining from database
+          var votesRemaining = currentUser.get("votesRemaining");
+          //calculate how many meeples user would have left after vote
+          var meeplesAfterVote = votesRemaining - voteNumber; 
+
+          //user will vote on game with a value 1-5 meeples
+          //before saving vote see if user has enough votes left
+          //if votes remaining is valid continue else alert user
+          if(meeplesAfterVote >= 0){
+
+            if(debug){console.log("You voted "+voteNumber+" on the game "+ gameNode.id + ". " + "You have " + meeplesAfterVote + " meeples left.");}
+
+            //create game object
+            var Games = Parse.Object.extend("Games");
+            //create query on game objects
+            var query = new Parse.Query(Games);
+
+            //find game object matching the id of the row that was clicked in the game table
+            query.get(gameNode.id, {
+              //if successful
+              success: function(games) {
+
+                //get previousVotes for this game from database
+                var previousVote = games.get("vote");
+                //if previousVote is undefined set to 0
+                if(!previousVote){previousVote=0;}
+                //alert user what they voted and thank them if they used all their votes
+                if(meeplesAfterVote != 0){
+                  alert("You voted "+voteNumber+" on the game " + games.get("game") + ". " + "You have " + meeplesAfterVote + " meeples left.");
+                }else{
+                  alert("You voted "+voteNumber+" on the game " + games.get("game") + ". " + "You have " + meeplesAfterVote + " meeples left. Thanks for voting, see you at Game Night!");
+                }
+
+                //creating game object
+                var gameObject = new Games();
+                //saving new vote total for that game
+                gameObject.save({objectId: gameNode.id, vote: previousVote + parseInt(voteNumber)});
+                //updating the game table with the new total
+                gameNode.getElementsByClassName('votes')[0].innerHTML = previousVote + parseInt(voteNumber);
+
+                //update votesRemaining for the currentUser
+                currentUser.set("votesRemaining", meeplesAfterVote);
+                //TODO: update what the user voted on user.set("gameVotes", {});
+                currentUser.save();
+              },
+              error: function(object, error) {
+                console.log(error);
+              }
+            });
+          } else {
+            alert("You only have enough meeples left to vote " + votesRemaining + " on this game. Otherwise, you will have to remove "+ (voteNumber-votesRemaining) +" meeples from other games to vote "+ voteNumber +" on this game.")
           }
-        });
+        } else {
+          alert("You need to login before you can vote!");
+        }
         
         //set meeples 1-vote as selected permenamntly
-        //update user vote counter
+        //reset voting values at the end of the voting period using cloud code
         
       };
     }
+};
+// use cloud code
+var votingReset = function () {
+  if(debug){console.log("votingReset");}
+  //get list of users
+  //cycle over the length of the array
+  //set votesRemaining to 5 for all users
+  var query = new Parse.Query(Parse.User);
+  query.find({
+      success: function(users) {
+        for( var i=0; i < users.length; i++ ){
+          users[i].set('votesRemaining', 5);
+          users[i].save();
+        }
+      }
+  });
+};
+
+var populateGameVotes = function() {
+  if(debug){console.log("populateGameVotes");}
+  var Games = Parse.Object.extend("Games");
+  var query = new Parse.Query(Games);
+  query.ascending("gameOrder");
+  query.find({
+    success: function(games) {
+      for( var i=0; i < games.length; i++ ){
+        document.getElementsByClassName('votes')[i].innerHTML = games[i].get("vote");
+      }
+    }
+  });
 };
 
 var populateUserList = function() {
@@ -137,7 +220,7 @@ var logout = function() {
 		Parse.User.logOut();
 		document.getElementById("login_btn_label").innerHTML = "You must login to vote!";
 		document.getElementById("login_button").innerHTML = "LogIn";
-    	document.getElementById("login_button").onclick = showDialog;
+    document.getElementById("login_button").onclick = showDialog;
 	};
 
 var toggleDialog = function() {
@@ -151,18 +234,17 @@ var toggleDialog = function() {
 }
 
 var register = function() {
-   if(debug){console.log("register");}
+  if(debug){console.log("register");}
 	var user = new Parse.User();
 	var username = document.getElementById("txtbox_gamer").value;
 	user.set("username", username);
 	user.set("password", document.getElementById("txtbox_password").value);
 	user.set("email", document.getElementById("txtbox_email").value);
+  user.set("votesRemaining", 5);
 	
-	/*user.set("votesRemaining", 5);
-	user.set("gameVotes", {});*/
-  
 	user.signUp(null, {
   		success: function(user) {
+        currentUser = Parse.User.current();
   			document.getElementById("login_btn_label").innerHTML = username;
   			document.getElementById("login_button").innerHTML = "Logout";
     		document.getElementById("login_button").onclick = logout;
@@ -189,11 +271,14 @@ after a game night:
  when we make the user list
  set the class according to user status
 */
+
+
 var login = function() {
    if(debug){console.log("login");}
 	var username = document.getElementById("txtbox_gamer").value;
 	Parse.User.logIn(username, document.getElementById("txtbox_password").value, {
   success: function(user) {
+    currentUser = Parse.User.current();
     document.getElementById("login_btn_label").innerHTML = username;
   	document.getElementById("login_button").innerHTML = "Logout";
     document.getElementById("login_button").onclick = logout;
